4970b4be011341de5cb396097959d2e336f266a6
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2021-10-14 17:21:31 +0300

    add HiddenPrints() class

---
+++ MyUtils/general.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+
+class HiddenPrints:
+    """
+    Function to prevent code printing.
+    Source: https://www.codegrepper.com/code-examples/python/python+turn+off+printing
+    Example:
+        with HiddenPrints():
+          print("This wont print")
+    """
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
