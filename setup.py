63fd803dc1cad04745dc94e8d0bba79c5bcc397a
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2021-10-14 15:00:25 +0300

    convert to package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MyUtils',
+    version='1.0',
+    packages=['MyUtils'],
+    url='https://github.com/Lin-Sinorodin/MyUtils',
+    license='MIT',
+    author='lin',
+    author_email='linsino123@gmail.com',
+    description='Common utils functions I use, stored in one place.'
+)
