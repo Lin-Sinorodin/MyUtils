85e3f7e12c7b1d5ecf70f9518932caa44be1e02f
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2021-10-16 13:45:47 +0300

    add install_latex() function

--- MyUtils/colab.py
+++ MyUtils/colab.py
@@ -1,15 +1,16 @@
+import os
+import sys
+
 class ColabUtils:
 
     @staticmethod
     def on_colab():
         """Check if the code is running on Google Colab. Source: https://stackoverflow.com/a/53586419"""
-        import sys
         return 'google.colab' in sys.modules
 
     @staticmethod
     def selenium_webdriver():
         """Selenium webdriver, works on Colab. Based on: https://github.com/korakot/kora/blob/master/kora/selenium.py"""
-        import os
         os.system('apt update')
         os.system('apt install chromium-chromedriver')
         os.system('pip install selenium')
@@ -21,3 +22,8 @@ class ColabUtils:
         options.add_argument('--disable-dev-shm-usage')
 
         return webdriver.Chrome('chromedriver', options=options)
+
+    @staticmethod
+    def install_latex():
+        print('=> Installing LaTeX, this might take a while...')
+        os.system('apt install texlive-fonts-recommended texlive-fonts-extra cm-super')
